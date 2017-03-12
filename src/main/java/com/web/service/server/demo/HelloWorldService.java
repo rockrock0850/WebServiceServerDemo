4b37e24c422da54a33751d8b688a8e442957297e
Adam <b9670094@gmail.com>
2017-03-12 19:59:10 +0800

    d

---
+++ src/main/java/com/web/service/server/demo/HelloWorldService.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2017 Far Eastone Telecommunications Co., Ltd.
+ * All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of 
+ * Far Eastone Telecommunications Co., Ltd. ("Confidential Information"). 
+ * 
+ * You shall not disclose such Confidential Information and shall use it 
+ * only in accordance with the terms of license agreement you entered
+ * into with Far Eastone Telecommunications Co., Ltd.
+ */
+
+package com.web.service.server.demo;
+
+/**
+ * 
+ * @author Adam
+ */
+public class HelloWorldService {
+	
+	public String sayHelloWorld(String name) {
+		return name;
+	}
+	
+}
